d0380fe3d5bb48551547a72c5227ecfcee4022f2
Michel9510 <98289449+Michel9510@users.noreply.github.com>
2022-04-26 18:38:25 -0400

    Update games.py

--- lib/games.py
+++ lib/games.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
-db = postgresqlDatabase('games', user='postgres',
+db = PostgresqlDatabase('michel', user='michel',
                         password='12345', host='localhost', port=5432)
 
 
@@ -11,28 +11,28 @@ class BaseModel(Model):
         database = db
 
 
-class Games(BaseModel):
-    title: CharField()
-    genre: CharField()
-    year: IntergerField()
-    description: CharField()
+class GamesList(BaseModel):
+    title = CharField()
+    genre = CharField()
+    year = IntegerField()
+    description = CharField()
 
 
 db.connect()
-db.drop_tables(Games)
-db.create_tables(Games)
+db.drop_tables([GamesList])
+db.create_tables([GamesList])
 
-Pokemon_Legend = Games(title='Pokemon Legend: Arceus', genre='Action role-playing', year='2022',
+Pokemon_Legend = GamesList(title='Pokemon Legend: Arceus', genre='Action role-playing', year='2022',
                        description='Pokémon Legends: Arceus is an action role-playing game that preserves the core gameplay of past entries in the main line series. The player is able to roam freely across the game map, divided into five large areas of individual biomes.')
-Pokemon_Unite = Games(title='Pokemon Unite', genre='Multiplayer online battle arena, real-time strategy', year='2021',
+Pokemon_Unite = GamesList(title='Pokemon Unite', genre='Multiplayer online battle arena, real-time strategy', year='2021',
                       description='Pokémon Unite is a multiplayer online battle arena game, with standard matches consisting of two teams of five players. Each match is time-limited, and the team with the highest total score by the end of each match wins. There is also quick battles, which has 4 people per team, and is 5 minutes long.')
-Call_of_Duty_MW2 = Games(title='Call of Duty: Modern Warfare 2', genre='RPG, Multiplayer, Action', year='2009',
+Call_of_Duty_MW2 = GamesList(title='Call of Duty: Modern Warfare 2', genre='RPG, Multiplayer, Action', year='2009',
                          description='Call of Duty: Modern Warfare 2 is a 2009 first-person shooter game developed by Infinity Ward and published by Activision. It is the sixth installment in the Call of Duty series and the direct sequel to Call of Duty 4: Modern Warfare.')
-Call_of_Duty_BO = Games(title='Call of Duty: Black Ops', genre='RPG, Multiplayer, Action', year='2010',
+Call_of_Duty_BO = GamesList(title='Call of Duty: Black Ops', genre='RPG, Multiplayer, Action', year='2010',
                         description='Call of Duty: Black Ops is a 2010 first-person shooter game developed by Treyarch and published by Activision. It was released worldwide in November 2010 for Microsoft Windows, the PlayStation 3, Wii, and Xbox 360, with a separate version for Nintendo DS developed by n-Space.')
-Battlefield_BC2 = Games(title='Battlefield: Bad Company 2', genre='RPG, Multiplayer, Action', year='2010',
+Battlefield_BC2 = GamesList(title='Battlefield: Bad Company 2', genre='RPG, Multiplayer, Action', year='2010',
                         description='Battlefield: Bad Company 2 is a first-person shooter video game developed by the Swedish firm DICE and published by Electronic Arts for Microsoft Windows, PlayStation 3, Xbox 360, iOS and Kindle Fire systems. It is a direct sequel to Battlefield: Bad Company and is part of the Battlefield game series.')
-Battlefield4 = Games(title='Battlefield 4', genre='RPG, Multiplayer, Action', year='2013',
+Battlefield4 = GamesList(title='Battlefield 4', genre='RPG, Multiplayer, Action', year='2013',
                      description='Battlefield 4™ features an intense and character-driven single player campaign, fused with the strongest elements of multiplayer. Pilot vehicles, take advantage of the dynamic destructible environments.')
 
 Pokemon_Legend.save()
@@ -45,44 +45,43 @@ Battlefield4.save()
 app = Flask(__name__)
 
 
-@app.route('/games.py',  methods=['GET', 'POST', 'PUT', 'DELETE'])
-@app.route('/games.py/<id>',  methods=['GET', 'POST', 'PUT', 'DELETE'])
-def games(id=None):
-    if request.method == 'GET':
+@app.route('/gameslists',  methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/gameslists/<id>',  methods=['GET', 'POST', 'PUT', 'DELETE'])
+# def games(id=None):
+   if request.method == 'GET':
         if id:
-            game = Games.get(Games.id == id)
+            game = GamesList.get(GamesList.id == id)
             game = model_to_dict(game)
             return jsonify(game)
         else:
             games = []
-            for game in Games.select():
+            for game in GamesList.select():
                 games.append(model_to_dict(game))
             return jsonify(games)
 
     if request.method == 'POST':
-        games = request.get_json()
-        games = dict_to_model(Games, games)
-        games.save()
-        games = model_to_dict(games)
-        games = jsonify(games)
-        return games
+        gameslist = request.get_json()
+        gameslist = dict_to_model(GamesList, gameslist)
+        gameslist.save()
+        gameslist = model_to_dict(gameslist)
+        gameslist = jsonify(gameslist)
+        return gameslist
 
     if request.method == 'DELETE':
-        games = Games.get(Games.id == id)
-        games.delete_instance()
+        gameslist = GamesList.get(GamesList.id == id)
+        gameslist.delete_instance()
         return jsonify({"deleted": True})
 
     if request.method == 'PUT':
         update_game = request.get_json()
-        games = Games.get(Games.id == id)
-        games.title = update_game['title']
-        games.genre = update_game['genre']
-        games.year = update_game['year']
-        games.description = update_game['description']
-        games.save()
-        games = model_to_dict(games)
-        games = jsonify(games)
-        return games
-
+        gameslist = GamesList.get(GamesList.id == id)
+        gameslist.title = update_game['title']
+        gameslist.genre = update_game['genre']
+        gameslist.year = update_game['year']
+        gameslist.description = update_game['description']
+        gameslist.save()
+        gameslist = model_to_dict(gameslist)
+        gameslist = jsonify(gameslist)
+        return gameslist
 
 app.run(port=3000, debug=True)
